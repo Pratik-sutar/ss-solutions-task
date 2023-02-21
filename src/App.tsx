6cec64ca3cb68f3514baf8e6911e7969bc038e30
pratik <pratiksutar99@gmail.com>
2023-02-21 22:44:19 +0530

    added

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React,{useState} from 'react';
+import {AiFillDelete} from 'react-icons/ai'
+import './App.css';
+let currId = 0;
+
+function App() {
+  const [list, setList] = useState<any[]>([])
+  const [task, setTask] = useState('')
+  const [last, setLast] = useState('')
+  return (
+    <div className="App">
+       <h1>Todos</h1>
+      <div className='inputField'>
+        <input type="text" required value={task} onChange={(e) => {
+          setTask(e.target.value)
+        }} />
+        <button onClick={() => {
+          if (task=="") {
+            alert("Please Enter a task name")
+          }else {
+          list.push({
+            "id": currId++,
+            "firstName": task,
+          })
+          setTask("")
+          setLast("")
+          console.log(list);
+        }}}>Add</button>
+      </div>
+      <div>
+        {list.map((item) => {
+          return (
+            <div key={item.id} className='listContainer' >
+              <div>
+              <p>{item.firstName} </p>
+              </div>
+              <div>
+                <p>
+                <AiFillDelete onClick={() => {
+                setList(list.filter(a =>
+                   a.id!==item.id
+                ))
+              }}/>
+                </p>
+              
+              </div>
+              
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
